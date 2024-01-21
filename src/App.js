90f45d08162fc6489ccab7fc095fc736034f3d22
Saikiran Kasturi <saikiran.kasturi35@gmail.com>
2024-01-22 01:57:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, { useContext } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import Intro from "./components/Intro/Intro";
+import Services from "./components/Services/Services";
+import Experience from "./components/Experience/Experience";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import { themeContext } from "./Context";
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div className="App">
-      Subscribe ZainKeepsCode
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <Navbar />
+      <Intro/>
+      <Services />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
